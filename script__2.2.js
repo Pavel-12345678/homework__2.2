9bea2a283ec6d4c47dc655f12aacf5c97b1f94d2
Pavel-12345678 <p1980n2014@yandex.ru>
2024-02-16 20:12:21 +0300

    first commit

---
+++ script__2.2.js
@@ -0,0 +1,67 @@
+let variable = 10;
+alert(variable);
+variable = 20;
+alert(variable);
+
+const year_release_first_iphon = 2007;
+alert(year_release_first_iphon);
+
+const name_creator_JavaScript = 'Брендан Эйх';
+alert(name_creator_JavaScript);
+
+let meaning__1 = 10;
+let meaning__2 = 2;
+let sum = meaning__1 + meaning__2; // 12 
+alert(sum);
+let difference = meaning__1 - meaning__2; // 8 
+alert(difference);
+let product = meaning__1 * meaning__2; // 20
+alert(product);
+let quotient = meaning__1 / meaning__2; // 5
+alert(quotient);
+
+let power = meaning__2 ** 5; // 32
+let result = power;
+alert(result);
+
+let meaning__3 = 9;
+let meaning__4 = 2;
+let remainder = 9 % 2; // 1
+alert (remainder);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+let age = prompt('Сколько Вам лет?');
+alert(age);
+
+let user = {
+    name: 'Павел',
+    age: 43,
+    isAdmin: true,
+    };
+
+user['city of residence'] = 'New York';
+
+user.age = 34;
+
+delete user['city of residence'];
+
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user[info]);
+
+let userName = prompt('Как Вас зовут?');
+alert(`Привет ${userName}!`);
+
+
+
+
+
+
+
